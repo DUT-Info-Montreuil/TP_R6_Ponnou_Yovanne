3b9b0c891e93e9a49476cd9bfc2330159439d42f
Yovanne <yovanne.ponnou@gmail.com>
2026-02-26 11:09:03 +0100

    init project java 17

---
+++ src/main/java/com/tp_blockchain_ponnou_yovanne/App.java
@@ -0,0 +1,13 @@
+package com.tp_blockchain_ponnou_yovanne;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
